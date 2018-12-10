2610c81f93784bb5babc7518775cf7d466cc99c0
kenjn04 <kenjn04@gmail.com>
2018-12-10 21:45:15 +0900

    Mon Dec 10 21:45:15 JST 2018

--- MyApplication/app/src/main/java/jp/co/nissan/hmi/myapplication/WidgetsListAdapter.kt
+++ MyApplication/app/src/main/java/jp/co/nissan/hmi/myapplication/WidgetsListAdapter.kt
@@ -1,12 +1,25 @@
 package jp.co.nissan.hmi.myapplication
 
+import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.widget.Adapter
+import android.view.ViewGroup
 import jp.co.nissan.hmi.myapplication.common.PackageItemInfo
 import jp.co.nissan.hmi.myapplication.common.WidgetItem
 import jp.co.nissan.hmi.myapplication.util.MultiHashMap
 
-class WidgetsListAdapter: Adapter<WidgetRowViewHolder> {
+class WidgetsListAdapter: RecyclerView.Adapter<WidgetRowViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): WidgetRowViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: WidgetRowViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
     fun setWidgets(model: MultiHashMap<PackageItemInfo, WidgetItem>) {
