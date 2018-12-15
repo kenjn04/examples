3b8afef73958b7e7bcef564b7693b833937db359
kenjn04 <kenjn04@gmail.com>
2018-12-16 00:41:10 +0900

    Sun Dec 16 00:41:10 JST 2018

--- MyApplication2/app/src/main/java/com/example/hmi/myapplication2/WidgetContainerView.kt
+++ MyApplication2/app/src/main/java/com/example/hmi/myapplication2/WidgetContainerView.kt
@@ -1,16 +1,13 @@
 package com.example.hmi.myapplication2
 
 import android.content.Context
+import android.os.AsyncTask
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.FrameLayout
 import android.widget.GridLayout
 import android.view.Gravity
 import android.view.MotionEvent
-import android.view.View
-import android.widget.LinearLayout
-
-
 
 class WidgetContainerView(
     context: Context,
@@ -23,8 +20,14 @@ class WidgetContainerView(
     private val widgetFrameWidth: Int
     private val widgetFrameHeight: Int
 
-    val params: GridLayout.LayoutParams
-        get() = this.layoutParams as GridLayout.LayoutParams
+    private val numX: Int = 4
+    private val numY: Int = 2
+
+    private var draggingWidget: WidgetFrame? = null
+
+    private val widgetList = mutableListOf<WidgetFrame?>(null, null, null, null, null, null, null, null)
+
+    private val widgetRearrangeTasks = mutableListOf<WidgetRearrangeTask>()
 
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
@@ -57,22 +60,75 @@ class WidgetContainerView(
         layoutParams.topMargin = y * widgetFrameHeight
         widget.layoutParams = layoutParams
         addView(widget)
+        for (i in 0..(widget.spanX - 1)) {
+            for (j in 0..(widget.spanY - 1)) {
+                val k = (y + j) * numX + x + i
+                widgetList[k] = widget
+            }
+        }
+    }
+
+    fun startWidgetDrag(widget: WidgetFrame) {
+        widgetRearrangeTasks.clear()
+        draggingWidget = widget
+    }
+
+    fun endWidgetDrag(): Boolean {
+        val layoutParams = draggingWidget!!.layoutParams as FrameLayout.LayoutParams
+        val centerWidth = layoutParams.leftMargin + (draggingWidget!!.spanX * widgetFrameWidth) / 2
+        val centerHeight = layoutParams.topMargin + (draggingWidget!!.spanY * widgetFrameHeight) / 2
+
+        if (moveWidget(draggingWidget!!, centerWidth / widgetFrameWidth, centerHeight / widgetFrameHeight)) {
+            commitWidgetRearrange()
+        }
+        draggingWidget = null
+
+        // TODO: Is there any case to return false?
+        return true
+    }
+
+    // TODO: Confirm how to move the widget
+    private fun moveWidget(widget: WidgetFrame, x: Int, y: Int): Boolean {
+        widgetRearrangeTasks.add(WidgetRearrangeTask(this, widget, x, y))
+        for (i in 0..(widget.spanX - 1)) {
+            for (j in 0..(widget.spanY - 1)) {
+                val k = (y + j) * numX + x + i
+                Log.d("aaaaabbbbb", k.toString() + " " + x.toString())
+                widgetRearrangeTasks.add(WidgetRearrangeTask(this, widgetList[k]!!, x + i, 1))
+            }
+        }
+        return true
+    }
+
+    private fun commitWidgetRearrange() {
+        for (task in widgetRearrangeTasks) {
+            task.execute()
+        }
+        widgetRearrangeTasks.clear()
+    }
+
+    class WidgetRearrangeTask(val container: WidgetContainerView, val widget: WidgetFrame, val x: Int, val y: Int)
+        : AsyncTask<Void, Void, Unit>() {
+
+        override fun doInBackground(vararg params: Void?) {
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            container.removeView(widget)
+            container.addWidget(widget, x, y)
+        }
     }
 
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
         when (ev!!.action) {
             MotionEvent.ACTION_MOVE -> {
-                Log.d("aaaaa2_i", ev.x.toString() + ev.y.toString())
+                Log.d("aaaaa2", ev.x.toString() + ev.y.toString())
             }
             MotionEvent.ACTION_DOWN -> {
-                Log.d("aaaaa2_i", "ACTION_DOWN")
-                performClick()
             }
             MotionEvent.ACTION_UP -> {
-                Log.d("aaaaa2_i", "ACTION_UP")
             }
             MotionEvent.ACTION_CANCEL -> {
-                Log.d("aaaaa2_i", "ACTION_CANCEL")
             }
         }
         return super.onInterceptTouchEvent(ev)
--- MyApplication2/app/src/main/java/com/example/hmi/myapplication2/WidgetFrame.kt
+++ MyApplication2/app/src/main/java/com/example/hmi/myapplication2/WidgetFrame.kt
@@ -2,10 +2,12 @@ package com.example.hmi.myapplication2
 
 import android.content.ClipData
 import android.content.Context
+import android.text.method.Touch
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.GridLayout
 
@@ -15,10 +17,18 @@ class WidgetFrame(
     defStyle: Int
 ): FrameLayout(context, attrs, defStyle) {
 
-    private var launcher: Launcher? = null
-
     var spanX: Int = 2
     var spanY: Int = 1
+
+    private var launcher: Launcher = context as Launcher
+
+    private var widgetContainer: WidgetContainerView
+
+    private var layoutParams: FrameLayout.LayoutParams? = null
+
+    private var originalLayoutParams: FrameLayout.LayoutParams? = null
+
+    private var originalTouchPosition: TouchPosition? = null
 
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
@@ -29,12 +39,63 @@ class WidgetFrame(
     }
 
     init {
-        if (context is Launcher) {
-            launcher = context as Launcher
-        }
+        widgetContainer = launcher.widgetContainer
+    }
+
+    override fun setLayoutParams(params: ViewGroup.LayoutParams?) {
+        layoutParams = params as LayoutParams
+        super.setLayoutParams(params)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        super.onTouchEvent(event)
+        when (event!!.action) {
+            MotionEvent.ACTION_MOVE -> {
+                movePositionByDrag(TouchPosition(event.x, event.y))
+            }
+            MotionEvent.ACTION_DOWN -> {
+                startDrag(event.x, event.y)
+            }
+            MotionEvent.ACTION_UP -> {
+                endDrag()
+            }
+        }
         return true
+    }
+
+    private fun movePositionByDrag(currentTouchPosition: TouchPosition) {
+        val touchPositionDiff = currentTouchPosition - originalTouchPosition!!
+        val newLayoutParams = FrameLayout.LayoutParams(originalLayoutParams).apply {
+            leftMargin += touchPositionDiff.left.toInt()
+            topMargin += touchPositionDiff.top.toInt()
+        }
+        super.setLayoutParams(newLayoutParams)
+    }
+
+    private fun startDrag(x: Float, y: Float) {
+        originalTouchPosition = TouchPosition(x, y)
+        originalLayoutParams = layoutParams
+
+        /** To move the widget most front */
+        widgetContainer.removeView(this)
+        widgetContainer.addView(this)
+
+        /** notify WidgetContainer to start drag with which widget */
+        widgetContainer.startWidgetDrag(this)
+    }
+
+    private fun endDrag() {
+        if (!widgetContainer.endWidgetDrag()) {
+            layoutParams = originalLayoutParams
+            super.setLayoutParams(layoutParams)
+        }
+        originalTouchPosition = null
+        originalLayoutParams = null
+    }
+
+    class TouchPosition(val left: Float, val top: Float) {
+        operator fun minus(position: TouchPosition): TouchPosition {
+            return TouchPosition(left - position.left, top - position.top)
+        }
     }
 }
