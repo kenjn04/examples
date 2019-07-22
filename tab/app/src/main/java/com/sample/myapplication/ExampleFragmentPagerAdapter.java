5f03e4872261a3c67b88e879230d5e304259c1e3
kenjn04 <kenjn04@gmail.com>
2019-07-23 00:00:54 +0900

    a

--- tab/app/src/main/java/com/sample/myapplication/ExampleFragmentPagerAdapter.java
+++ tab/app/src/main/java/com/sample/myapplication/ExampleFragmentPagerAdapter.java
@@ -18,13 +18,19 @@ public class ExampleFragmentPagerAdapter extends FragmentPagerAdapter {
                 return ExampleFragment.newInstance(android.R.color.holo_green_light);
             case 2:
                 return ExampleFragment.newInstance(android.R.color.holo_red_dark);
+            case 3:
+                return ExampleFragment.newInstance(android.R.color.holo_blue_bright);
+            case 4:
+                return ExampleFragment.newInstance(android.R.color.holo_green_light);
+            case 5:
+                return ExampleFragment.newInstance(android.R.color.holo_red_dark);
         }
         return null;
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 6;
     }
 
     @Override
--- tab/app/src/main/java/com/sample/myapplication/MainActivity.kt
+++ tab/app/src/main/java/com/sample/myapplication/MainActivity.kt
@@ -3,14 +3,29 @@ package com.sample.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewGroupCompat
 import android.support.v4.view.ViewPager
+import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setViews()
+
+        btn.setOnClickListener {
+            val current = tab.scrollX / 200
+            if ((count - current) > 3) {
+                tab.scrollX += 200
+            }
+        }
     }
 
     private fun setViews() {
@@ -18,7 +33,24 @@ class MainActivity : AppCompatActivity() {
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
         val adapter = ExampleFragmentPagerAdapter(manager)
         viewPager.adapter = adapter
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        tabLayout.setupWithViewPager(viewPager)
+
+        count = adapter.count
+        (0..(count - 1)).forEach {
+            val i = it
+            Log.d("aaaaa", "${it} ${adapter.getPageTitle(it)}")
+            val textView = TextView(this)
+            textView.text = adapter.getPageTitle(it)
+            textView.textSize = 30F
+            textView.gravity = Gravity.CENTER
+            val param = ViewGroup.LayoutParams(
+                200,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            tab.addView(textView, param)
+            textView.setOnClickListener {
+                viewPager.setCurrentItem(i, false)
+            }
+        }
+
     }
 }
